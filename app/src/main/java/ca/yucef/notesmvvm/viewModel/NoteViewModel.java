573b2dff69df61c1812e179a13b2dc4c407f4344
Ahmed Yucef <ahmed.yucef@videri.com>
2018-11-22 10:49:27 -0500

    Initial commit

---
+++ app/src/main/java/ca/yucef/notesmvvm/viewModel/NoteViewModel.java
@@ -0,0 +1,42 @@
+package ca.yucef.notesmvvm.viewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import ca.yucef.notesmvvm.model.Note;
+import ca.yucef.notesmvvm.model.NoteRepository;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
